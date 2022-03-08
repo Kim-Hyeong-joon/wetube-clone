5b9c831fbafd17d6f64e8c936e9b79b4e83f4184
Kim-Hyeong-joon <gudwns1020@naver.com>
2022-03-08 15:50:51 +0900

    Response

--- src/server.js
+++ src/server.js
@@ -4,11 +4,18 @@ const PORT = 4000;
 
 const app = express(); // application 생성
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => {
+  return res.send("I still love you."); //request를 받았으니, response를 반환(return)해야한다.
+};
+
+const handleLogin = (req, res) => {
+  return res.send("this is log-in page");
+};
 
 app.get("/", handleHome); // get request 발생 시 실행하는 callback 함수.
+app.get("/login", handleLogin);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+  console.log(`✅Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening); // 외부접속을 Listening 한다.
