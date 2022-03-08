89038ab61014cc035a29194440fb7927769b0791
Kim-Hyeong-joon <gudwns1020@naver.com>
2022-03-08 15:04:37 +0900

    Get Request

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ import express from "express";
 
 const PORT = 4000;
 
-const app = express();
+const app = express(); // application 생성
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+const handleHome = () => console.log("Somebody is trying to go home.");
 
-app.listen(PORT, handleListening)
+app.get("/", handleHome); // get request 발생 시 실행하는 callback 함수.
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening); // 외부접속을 Listening 한다.
