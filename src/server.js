be0bc0917b36eb5f710470464250626d8ce08408
Kim-Hyeong-joon <gudwns1020@naver.com>
2022-03-10 00:20:46 +0900

    Middleware

--- src/server.js
+++ src/server.js
@@ -4,16 +4,27 @@ const PORT = 4000;
 
 const app = express(); // application 생성
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue");
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("I still love you."); //request를 받았으니, response를 반환(return)해야한다.
+  return res.send("end");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("this is log-in page");
-};
-
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome); // get request 발생 시 실행하는 callback 함수.
-app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✅Server listening on port http://localhost:${PORT} 🚀`);
