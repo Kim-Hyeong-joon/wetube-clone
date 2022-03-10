41b4af78ea7caf96727c776cfcdac36d46d8b56b
김형준 <gudwns1020@naver.com>
2022-03-11 01:19:52 +0900

    Routers

--- src/server.js
+++ src/server.js
@@ -1,23 +1,44 @@
 import express from "express";
+import { handle } from "express/lib/application";
 import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express(); // application 생성
 const logger = morgan("dev");
+app.use(logger);
 
-const handleLogin = (req, res) => {
-  return res.send("login");
-};
+const globalRouter = express.Router();
 
 const handleHome = (req, res) => {
-  console.log("I will respond");
-  return res.send("hello");
+  return res.send("Home");
 };
 
-app.use(logger);
-app.get("/", handleHome); // get request 발생 시 실행하는 callback 함수.
-app.get("/login", handleLogin);
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => {
+  return res.send("Edit User");
+};
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => {
+  return res.send("Watch Video");
+};
+
+videoRouter.get("/watch", handleWatchVideo);
+
+const handleVideo = (req, res) => {
+  return res.send("/video and then all");
+};
+
+app.use("/", globalRouter);
+app.get("/videos", handleVideo);
+app.use("/user", userRouter);
 
 const handleListening = () =>
   console.log(`✅Server listening on port http://localhost:${PORT} 🚀`);
