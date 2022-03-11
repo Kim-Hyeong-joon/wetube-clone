d9977588a5c4dc742c78c898013b7812c7b39da7
김형준 <gudwns1020@naver.com>
2022-03-11 18:36:36 +0900

    URL Parameters

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,15 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch Videos");
-export const edit = (req, res) => res.send("Edit Videos");
+export const see = (req, res) => {
+  console.log(req.params);
+  return res.send("See Videos");
+};
+export const edit = (req, res) => {
+  console.log(req.params);
+  return res.send("Edit Videos");
+};
+export const search = (req, res) => res.send("Search Videos");
+export const upload = (req, res) => res.send("Upload Videos");
+export const deleteVideo = (req, res) => {
+  console.log(req.params);
+  return res.send("Delete Videos");
+};
