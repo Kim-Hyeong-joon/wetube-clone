2d2a3e2f4334278d5d24ba7e11b9c3d8f87504f4
김형준 <gudwns1020@naver.com>
2022-03-12 14:23:01 +0900

    Pug

--- src/server.js
+++ src/server.js
@@ -6,10 +6,13 @@ import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
+console.log(process.cwd());
+
 const app = express(); // application 생성
 const logger = morgan("dev");
 app.use(logger);
 
+app.set("view engine", "pug");
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
