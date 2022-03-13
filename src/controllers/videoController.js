3b3b692c22c06cb711f4850bc4324018093042e9
김형준 <gudwns1020@naver.com>
2022-03-13 21:38:27 +0900

    Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,7 @@
 export const trending = (req, res) => res.render("home");
 
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send("See Videos");
-};
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit Videos");
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Videos");
 export const upload = (req, res) => res.send("Upload Videos");
 export const deleteVideo = (req, res) => {
