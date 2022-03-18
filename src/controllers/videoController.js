0e93e34a0c18d06d498ad929b996ca2fd6010361
김형준 <gudwns1020@naver.com>
2022-03-18 22:12:11 +0900

    Edit video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
     rating: 5,
     comments: 2,
     creatAt: "2 minutes ago",
-    views: 59,
+    views: 1,
     id: 1,
   },
   {
@@ -28,15 +28,18 @@ let videos = [
 export const trending = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
+export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  res.render("watch", { pageTitle: `Watching ${video.title}` });
+  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search Videos");
-export const upload = (req, res) => res.send("Upload Videos");
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("Delete Videos");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  console.log(req.body);
+  return res.redirect(`/videos/${id}`);
 };
