637d302a537ebcdca9d1da233de624c4f1c5ba3f
김형준 <gudwns1020@naver.com>
2022-07-01 16:56:49 +0900

    Edit video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,9 +36,19 @@ export const getEdit = async (req, res) => {
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." }); // video가 null 인 경우 에러메세지 출력
+  }
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+  await video.save();
   return res.redirect(`/videos/${id}`);
 };
 
