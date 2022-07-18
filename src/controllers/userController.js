7ee7205df92bc4e92fe7b80c370b9ddb250b441c
김형준 <gudwns1020@naver.com>
2022-07-18 17:17:10 +0900

    File Uploads part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -146,12 +146,12 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
     file,
   } = req;
-  console.log(file);
+
   if (email !== req.session.user.email) {
     const exists = await User.exists({ email });
     if (exists) {
@@ -172,9 +172,12 @@ export const postEdit = async (req, res) => {
     }
   }
 
+  console.log(file);
+
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
