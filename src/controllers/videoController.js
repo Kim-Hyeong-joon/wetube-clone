59e75bc4c7c295e7befa16bbf74b3e80c341b374
김형준 <gudwns1020@naver.com>
2022-08-05 00:06:34 +0900

    Realtime Comments

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   const videos = await Video.find({})
@@ -9,8 +10,7 @@ export const home = async (req, res) => {
 };
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
-
+  const video = await Video.findById(id).populate("owner").populate("comments");
   if (video === null) {
     return res.status(404).render("404", { pageTitle: "Video not found." }); // video가 null 인 경우 에러메세지 출력
   }
@@ -129,12 +129,25 @@ export const registerView = async (req, res) => {
   return res.sendStatus(200);
 };
 
-export const createComment = (req, res) => {
+export const createComment = async (req, res) => {
   const {
     params: { id },
     body: { text },
     session: { user },
   } = req;
-  console.log(user, text, id);
-  return res.end();
+
+  const video = await Video.findById(id);
+
+  if (!video) {
+    return res.sendStatus(404);
+  }
+
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+  video.comments.push(comment._id);
+  video.save();
+  return res.sendStatus(201);
 };
